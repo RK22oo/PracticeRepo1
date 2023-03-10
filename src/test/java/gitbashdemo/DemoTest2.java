884b8b68aa785e02f91f51ae43358b2161ac4188
RK22oo <rocky1.uthappa@gmail.com>
2023-03-10 13:08:28 +0530

    updated

--- src/test/java/gitbashdemo/DemoTest2.java
+++ src/test/java/gitbashdemo/DemoTest2.java
@@ -2,13 +2,14 @@ package gitbashdemo;
 
 import org.testng.annotations.Test;
 
-public class DemoTest {
+public class DemoTest2 {
+
 	@Test
 	public void test1() {
-		System.out.println("DemoTest ==>test1");
+		System.out.println("DemoTest2 ==>test1");
 	}
 	@Test
 	public void test2() {
-		System.out.println("DemoTest ==>test2");
+		System.out.println("DemoTest2 ==>test2");
 	}
 }
